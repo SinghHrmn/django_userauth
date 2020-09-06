0cbdec499abcdc235beba6c301b80b4bae368497
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-09-06 22:26:35 +0530

    updated views

--- users/views.py
+++ users/views.py
@@ -4,30 +4,33 @@ from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
-def index(request):
+def index_view(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
     return render(request, 'users/index.html')
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        
+
         user = authenticate(request, username=username, password=password)
 
         if user:
             login(request, user)
             return HttpResponseRedirect(reverse('index'))
         else:
-            return render(request, 'users/login', {
+            return render(request, 'users/login.html', {
                 "message": "Invalid Credentials"
             })
         
     return render(request, 'users/login.html')
 
-def logout(request):
-    pass
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "Logged Out"
+    })
 
 
 
