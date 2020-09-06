8bbb53e0b99640dc5505edf176f75e809f652559
Harmandeep Singh <singh.hrmn98@gmail.com>
2020-09-06 20:47:30 +0530

    updated the django-app

---
+++ users/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+
+
+# Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request, 'users/index.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, 'users/login', {
+                "message": "Invalid Credentials"
+            })
+        
+    return render(request, 'users/login.html')
+
+def logout(request):
+    pass
+
+
+
